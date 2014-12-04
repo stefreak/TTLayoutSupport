7503c7ba769896a5a4096e8ce608eb0423e36aff
Steffen Neubauer <stefreak@googlemail.com>
2014-12-04 03:56:14 +0100

    correct reference images and more tests

--- Example/Tests/TTLayoutSupportSnapshotTest.m
+++ Example/Tests/TTLayoutSupportSnapshotTest.m
@@ -15,11 +15,15 @@
 
 #define V(__identifier) ([NSString stringWithFormat:@"%@-%@", __identifier, [[UIDevice currentDevice] systemVersion]])
 
+
+
 @interface TTLayoutSupportSnapshotTest ()
 
 @property (nonatomic, strong) UIView *viewToTest;
 
-@property (nonatomic, strong) TTDemoChildViewController *testViewController;
+@property (nonatomic, strong) TTDemoChildViewController *withoutScrollView;
+
+@property (nonatomic, strong) TTDemoScrollViewController *plainScrollView;
 
 @end
 
@@ -31,10 +35,10 @@
 
     //self.recordMode = YES;
     
-    self.viewToTest = [UIView new];
-    self.viewToTest.frame = CGRectMake(0, 0, 320, 640);
+    self.viewToTest = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 640)];
     
-    self.testViewController = [[TTDemoChildViewController alloc] init];
+    self.withoutScrollView = [[TTDemoChildViewController alloc] init];
+    self.plainScrollView = [[TTDemoScrollViewController alloc] init];
     
     // fails sometimes without it
     self.renderAsLayer = YES;
@@ -42,41 +46,83 @@
 
 - (void)addController:(UIViewController *)controller
 {
+    controller.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     [self.viewToTest addSubview:controller.view];
     controller.view.frame = self.viewToTest.bounds;
+    
+//    self.viewToTest = controller.view;
+//    self.viewToTest.frame = CGRectMake(0, 0, 320, 640);
+//    [self.viewToTest setNeedsLayout];
 }
+
+#pragma mark - without scrollview
 
 - (void)testTopLayoutGuideWithoutScrollview
 {
-    [self addController:self.testViewController];
+    [self addController:self.withoutScrollView];
 
-    self.testViewController.tt_bottomLayoutGuideLength = 0;
+    self.withoutScrollView.tt_bottomLayoutGuideLength = 0;
     
-    self.testViewController.tt_topLayoutGuideLength = 0;
-    FBSnapshotVerifyView(self.viewToTest, V(@"top 0, bottom 0"));
+    self.withoutScrollView.tt_topLayoutGuideLength = 0;
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 0, bottom 0 - w_o scrollview"));
     
-    self.testViewController.tt_topLayoutGuideLength = 50;
-    FBSnapshotVerifyView(self.viewToTest, V(@"top 50, bottom 0"));
+    self.withoutScrollView.tt_topLayoutGuideLength = 50;
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 50, bottom 0 - w_o scrollview"));
 }
 
 - (void)testBottomLayoutGuideWithoutScrollview
 {
-    [self addController:self.testViewController];
+    [self addController:self.withoutScrollView];
     
-    self.testViewController.tt_topLayoutGuideLength = 0;
-    self.testViewController.tt_bottomLayoutGuideLength = 100;
+    self.withoutScrollView.tt_topLayoutGuideLength = 0;
+    self.withoutScrollView.tt_bottomLayoutGuideLength = 100;
     
-    FBSnapshotVerifyView(self.viewToTest, V(@"top 0, bottom 100"));
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 0, bottom 100 - w_o scrollview"));
 }
 
 - (void)testBothWithoutScrollView
 {
-    [self addController:self.testViewController];
+    [self addController:self.withoutScrollView];
     
-    self.testViewController.tt_topLayoutGuideLength = 200;
-    self.testViewController.tt_bottomLayoutGuideLength = 100;
+    self.withoutScrollView.tt_topLayoutGuideLength = 200;
+    self.withoutScrollView.tt_bottomLayoutGuideLength = 100;
     
-    FBSnapshotVerifyView(self.viewToTest, V(@"top 200, bottom 100"));
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 200, bottom 100 - w_o scrollview"));
+}
+
+#pragma mark - plan scrollview
+
+- (void)testTopLayoutGuideWithPlainScrollview
+{
+    [self addController:self.plainScrollView];
+    
+    self.plainScrollView.tt_bottomLayoutGuideLength = 0;
+    
+    self.plainScrollView.tt_topLayoutGuideLength = 0;
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 0, bottom 0 - plain scrollview"));
+    
+    self.plainScrollView.tt_topLayoutGuideLength = 50;
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 50, bottom 0 - plain scrollview"));
+}
+
+- (void)testBottomLayoutGuideWithPlainScrollview
+{
+    [self addController:self.plainScrollView];
+    
+    self.plainScrollView.tt_topLayoutGuideLength = 0;
+    self.plainScrollView.tt_bottomLayoutGuideLength = 100;
+    
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 0, bottom 100 - plain scrollview"));
+}
+
+- (void)testBothWithPlainScrollview
+{
+    [self addController:self.plainScrollView];
+    
+    self.plainScrollView.tt_topLayoutGuideLength = 200;
+    self.plainScrollView.tt_bottomLayoutGuideLength = 100;
+    
+    FBSnapshotVerifyView(self.viewToTest, V(@"top 200, bottom 100 - plain scrollview"));
 }
 
 @end
