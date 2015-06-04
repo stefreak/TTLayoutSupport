a288eab994dbcec48f72e14e84c24f5b16168c4a
Steffen Neubauer <stefreak@googlemail.com>
2015-06-04 20:21:30 +0200

    Add some updated documentation

--- Pod/Classes/UIViewController+TTLayoutSupport.h
+++ Pod/Classes/UIViewController+TTLayoutSupport.h
@@ -9,10 +9,29 @@
 #import <UIKit/UIKit.h>
 #import "TTLayoutGuide.h"
 
+/**
+ *  The `UIViewController+TTLayoutSupport` category makes it very easy to change
+ *  either the position of a `UIViewController`s `topLayoutGuide` or `bottomLayoutGuide`.
+ *
+ *  The ViewController will automatically adjust scroll view insets according to it's
+ *  top- or bottom layout guides.
+ *
+ *  If you don't want that, set `automaticallyAdjustsScrollViewInsets` to `NO`.
+ * 
+ *  @see TTLayoutGuide
+ */
 @interface UIViewController (TTLayoutSupport)
 
+/**
+ *  Returns an instance representing the top layout guide.
+ *  Can be used to change the position of the top layout guide.
+ */
 @property (nonatomic, readonly) TTLayoutGuide* tt_topLayoutGuide;
 
+/**
+ *  Returns an instance representing the bottom layout guide.
+ *  Can be used to change the position of the bottom layout guide.
+ */
 @property (nonatomic, readonly) TTLayoutGuide* tt_bottomLayoutGuide;
 
 @end
